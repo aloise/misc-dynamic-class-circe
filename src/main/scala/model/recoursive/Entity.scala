6f62468f111022f5ad28f71a4fd3a8e8a7587660
Ihor <erwaht@gmail.com>
2018-08-29 21:01:11 +0200

    recursive schema tests - coalgebra

--- src/main/scala/model/recoursive/Entity.scala
+++ src/main/scala/model/recoursive/Entity.scala
@@ -5,8 +5,10 @@ import matryoshka.implicits._
 import scalaz._
 import Scalaz._
 import cats.data.Nested
-import io.circe.{Json, JsonObject}
-import matryoshka.Algebra
+import io.circe.Decoder.Result
+import io.circe.Json._
+import io.circe._
+import matryoshka.{Algebra, Coalgebra}
 
 import scala.reflect.runtime.universe._
 
@@ -19,7 +21,8 @@ case class StringField[R](value:String) extends Field[R]{ type V = String}
 case class IntField[R](value: Int) extends Field[R]{ type V = Int }
 case class BooleanField[R](value: Boolean) extends Field[R]{ type V = Boolean}
 case class RecordSet[R](value: Map[String,R]) extends Field[R]{ type V = Map[String, R]}
-
+case class ArrayField[R](value:Vector[R]) extends Field[R]{ type V = Vector[R]}
+case class EmptyField[R]() extends Field[R]{ type V = Nothing ; def value:Nothing = ??? }
 
 object Entity {
   type Record = Fix[Field]
@@ -29,7 +32,10 @@ object Entity {
       case StringField(v) => Applicative[G].point(StringField(v))
       case IntField(v) => Applicative[G].point(IntField(v))
       case BooleanField(v) => Applicative[G].point(BooleanField(v))
+      case ArrayField(fields) => fields.traverse(x => f(x)).map(ArrayField(_))
       case RecordSet(fields) => fields.traverse(x => f(x)).map(RecordSet(_))
+      case EmptyField() => Applicative[G].point(EmptyField())
+
     }
   }
 
@@ -43,15 +49,29 @@ object Entity {
 
     implicit val stringToCirceKey: ToCirceKey[String] = (value: String) => value
 
-    val evaluateJson : Algebra[Field, Json] = {
+    val algebraFieldJson : Algebra[Field, Json] = {
       case StringField(v) => Json.fromString(v)
       case IntField(v) => Json.fromInt(v)
       case BooleanField(v) => Json.fromBoolean(v)
       case RecordSet(fields) => Json.obj(fields.toList:_*)
+      case ArrayField(values) => Json.arr(values:_*)
+      case EmptyField() => Json.Null
     }
 
+    val coalgebraFieldJson : Coalgebra[Field, Json] = (json:Json) => json.fold(
+      EmptyField(),
+      jsonBoolean => BooleanField(jsonBoolean),
+      jsonNumber => IntField(jsonNumber.toInt.getOrElse(0)),
+      jsonString => StringField(jsonString),
+      jsonArray => ArrayField(jsonArray),
+      jsonObject => RecordSet(jsonObject.toMap)
+    )
+
     implicit def toCirceEncoder:Encoder[Record] =
-      (a: Record) => a.cata(evaluateJson)
+      (a: Record) => a.cata(algebraFieldJson)
+
+    implicit def toCirceDecoder:Decoder[Record] =
+      (a: HCursor) => ???
 
   }
 
@@ -62,5 +82,6 @@ object Entity {
   def intField(value:Int): Record = embed(IntField[Record](value))
   def booleanField(value:Boolean): Record = embed(BooleanField[Record](value))
   def record(fields:(String,Record)*): Record = embed(RecordSet[Record](fields.toMap))
+  def array(fields:Record*): Record = embed(ArrayField[Record](fields.toVector))
 
 }
